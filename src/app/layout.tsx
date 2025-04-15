a0b7157be80f951dcc7d2c38773ad432a17c755e
tomaslami <tomaslamiguralnik@gmail.com>
2025-04-15 18:10:38 -0300

    feat: add Google Tag Manager integration and create robots.txt and sitemap.xml files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import Navbar from "@/components/NavBar"
 import Footer from "@/components/Footer";
 import FloatingButton from "@/components/FloatingButton";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -23,9 +24,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5T2X2KLL');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`${montserrat.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5T2X2KLL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         <main className="max-w-[1680px] mx-auto">{children}</main>
         <Footer/>
